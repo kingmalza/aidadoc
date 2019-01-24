b9e8be04569332f3b6fafe05d94e5dc0e3d278d7
kingmalza <alessandro.malzanini@gmail.com>
2019-01-24 14:58:20 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -90,6 +90,12 @@ html_theme_options = {
     'titles_only': False
 }
 
+html_context = {
+    "display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+    "last_updated": True,
+    "commit": False,
+}
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
